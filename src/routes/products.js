7f5934c46c3baed041c0afeb5dc3f904bd8456bc
botechiaeri <botechiaerikaluisa@gmail.com>
2022-05-10 06:44:24 -0300

    Update products.js

--- src/routes/products.js
+++ src/routes/products.js
@@ -2,6 +2,19 @@
 const productsControllers = require('./../controllers/productsControllers.js')
 const express = require('express')
 const router = express.Router()
+
+/*/                                    Home
+/products                    Muestra Productos
+/products/create       Creación Producto
+/products/detail/:id   Muestra Detalle Producto
+/products/edit/:id      Edición Producto
+/products/:id              Eliminación Producto
+
+
+* Mostrar la información que corresponda según el contexto y la sección.
+* Enviar los datos por el método correspondiente
+* Redireccionar a la sección que corresponda según el contexto
+*/
 router.get('/', productsControllers.homeTelos)
 router.get('/create', productsControllers.createTelos)
 router.get('/comuna-:idC', productsControllers.detailsByIdC)
